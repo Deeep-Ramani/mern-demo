59758a046923da78a772ccbc11092958d9299518
Deeep-Ramani <deep.r@simformsolutions.com>
2023-12-12 22:05:05 +0530

    Create node demo

---
+++ frontend/src/Constants/HttpMethods.js
@@ -0,0 +1,7 @@
+export const POST_METHOD = {
+    userListSearchSort: 'http://localhost:7000/user/getUserListSearchSort',
+    addUser: 'http://localhost:7000/user/addUser',
+}
+
+export const GET_METHOD = {
+}
